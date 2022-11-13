03570d9a81a7c7c879f4e330f05f420646be532b
steve85 <sdryan85+github@gmail.com>
2022-11-14 07:22:38 +1100

    Adds code

---
+++ Source/SteveLabs.DockerBuildTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SteveLabs.DockerBuildTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
