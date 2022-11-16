58fe913a056b29d9181f753988c980104b93ac35
steve85 <sdryan85+github@gmail.com>
2022-11-16 15:38:04 +1100

    Adds unit tests

--- Source/SteveLabs.DockerBuildTest.MSTest/GreetingServiceTests.cs
+++ Source/SteveLabs.DockerBuildTest.MSTest/GreetingServiceTests.cs
@@ -14,5 +14,47 @@ namespace SteveLabs.DockerBuildTest.MSTest
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void GivenGreetTo_WhenToArgumentNull_ThenThrowsArgumentNullException()
+        {
+            var target = new GreetingService();
+
+            Assert.ThrowsException<ArgumentNullException>(() => target.GreetTo(null));
+        }
+
+        [TestMethod]
+        public void GivenGreetTo_WhenToArgumentPassed_ThenGreetingReturned()
+        {
+            var target = new GreetingService();
+            var expected = "Hello to you Billy";
+            var actual = target.GreetTo("Billy");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenGreetFrom_WhenFromArgumentNull_ThenThrowsArgumentNullException()
+        {
+            var target = new GreetingService();
+
+            Assert.ThrowsException<ArgumentNullException>(() => target.GreetFrom(null));
+        }
+
+        [TestMethod]
+        public void GivenGreetToFrom_WhenToArgumentNull_ThenThrowsArgumentNullException()
+        {
+            var target = new GreetingService();
+
+            Assert.ThrowsException<ArgumentNullException>(() => target.GreetToFrom(null, "from"));
+        }
+
+        [TestMethod]
+        public void GivenGreetToFrom_WhenFromArgumentNull_ThenThrowsArgumentNullException()
+        {
+            var target = new GreetingService();
+
+            Assert.ThrowsException<ArgumentNullException>(() => target.GreetToFrom("from", null));
+        }
     }
 }
