be65fabc8f4a58ff9c3c66c71338c9c2c0d1cb8d
steve85 <sdryan85+github@gmail.com>
2023-08-01 21:30:34 +1000

    Unit tests passing

--- Source/SteveLabs.DockerBuildTest/Service/GreetingService.cs
+++ Source/SteveLabs.DockerBuildTest/Service/GreetingService.cs
@@ -9,16 +9,36 @@
 
         public string GreetTo(string to)
         {
+            if (to is null)
+            {
+                throw new ArgumentNullException(nameof(to));
+            }
+
             return $"Hello to you {to}";
         }
 
         public string GreetFrom(string from)
         {
+            if (from is null)
+            {
+                throw new ArgumentNullException(nameof(from));
+            }
+
             return $"Hello from {from}";
         }
 
         public string GreetToFrom(string to, string from)
         {
+            if (to is null)
+            {
+                throw new ArgumentNullException(nameof(to));
+            }
+
+            if (from is null)
+            {
+                throw new ArgumentNullException(nameof(from));
+            }
+
             return $"Hello to you {to} from {from}";
         }
     }
